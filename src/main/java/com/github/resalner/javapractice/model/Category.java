335dd877a736a86bbea56a4967cb2076c98566b9
KING <victorzakharich@gmail.com>
2024-07-04 13:59:12 +0300

    исправил

--- src/main/java/com/github/resalner/javapractice/model/Category.java
+++ src/main/java/com/github/resalner/javapractice/model/Category.java
@@ -1,9 +1,11 @@
 package com.github.resalner.javapractice.model;
 
 import jakarta.persistence.*;
-
+import lombok.*;
 @Entity
 @Table(name = "category")
+@Data
+@ToString(includeFieldNames=true)
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
