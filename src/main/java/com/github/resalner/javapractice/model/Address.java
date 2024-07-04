f55c42d13ba39f271a8d2d050d0786d564198846
KING <victorzakharich@gmail.com>
2024-07-04 13:32:41 +0300

    Добавил класс адреса.

--- src/main/java/com/github/resalner/javapractice/model/Address.java
+++ src/main/java/com/github/resalner/javapractice/model/Address.java
@@ -1,4 +1,26 @@
 package com.github.resalner.javapractice.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "address")
+@Data
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "house_number")
+    private String house_number;
+
+    @Column(name = "apartment_number")
+    private String apartment_number;
 }
