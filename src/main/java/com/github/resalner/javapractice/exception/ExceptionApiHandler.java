c21ba18ad0d5f4d0d3c6f503ec5c810ee7ef089f
Resalner <resalner@gmail.com>
2024-07-10 09:35:27 +0300

    Update ExceptionApiHandler.java

--- src/main/java/com/github/resalner/javapractice/exception/ExceptionApiHandler.java
+++ src/main/java/com/github/resalner/javapractice/exception/ExceptionApiHandler.java
@@ -1,18 +1,23 @@
 package com.github.resalner.javapractice.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-@RestControllerAdvice
-public class ExceptionApiHandler {
+@ControllerAdvice
+public class GlobalExceptionHandler {
 
-	@ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorMessage> unexpeсtedException(Exception exception) {
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ErrorMessage(exception.getMessage()));
+    @ExceptionHandler(value = NoSuchUserExistsException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public @ResponseBody ErrorResponse handleException(NoSuchUserExistsException ex) {
+        return new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
     }
-	
+
+    @ExceptionHandler(value = UserAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public @ResponseBody ErrorResponse handleException(UserAlreadyExistsException ex) {
+        return new ErrorResponse(HttpStatus.CONFLICT.value(), ex.getMessage());
+    }
 }
