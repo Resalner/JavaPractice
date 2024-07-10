20a39cb576a4b13bddee58debe6549c5d0abc8b4
Resalner <resalner@gmail.com>
2024-07-10 09:24:20 +0300

    Update ErrorResponse.java

--- src/main/java/com/github/resalner/javapractice/exception/ErrorResponse.java
+++ src/main/java/com/github/resalner/javapractice/exception/ErrorResponse.java
@@ -1,19 +1,20 @@
 package com.github.resalner.javapractice.exception;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ErrorResponse {
 	
-	private String response;
-
-	public ErrorResponse(String message) {
-		this.response = response;
-	}
-
-	public String getResponse() {
-		return response;
-	}
-
-	public void setResponse(String response) {
-		this.response = response;
+	private int statusCode;
+    	private String message;
+	
+	public ErrorResponse(String message)
+	{
+		super();
+		this.message = message;
 	}
 	
 }
