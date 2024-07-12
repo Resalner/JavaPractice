55864475fc5988cd405bc62926130a4bcfb61185
ilya <99881940+Kuchikamba@users.noreply.github.com>
2024-07-12 08:58:48 +0300

    Add files via upload

--- src/main/java/com/github/resalner/javapractice/exception/NotFoundException.java
+++ src/main/java/com/github/resalner/javapractice/exception/NotFoundException.java
@@ -3,7 +3,8 @@ package com.github.resalner.javapractice.exception;
 public class NotFoundException extends RuntimeException {
     private String message;
 
-    public NotFoundException() {}
+    public NotFoundException() {
+    }
 
     public NotFoundException(String msg) {
         super(msg);
