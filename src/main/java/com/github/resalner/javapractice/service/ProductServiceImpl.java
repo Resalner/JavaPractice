53b8b22ca04230cc6eaf1963165bf0d8d711793c
ilya <99881940+Kuchikamba@users.noreply.github.com>
2024-07-12 16:19:32 +0300

    Add files via upload

---
+++ src/main/java/com/github/resalner/javapractice/service/ProductServiceImpl.java
@@ -0,0 +1,56 @@
+package com.github.resalner.javapractice.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.github.resalner.javapractice.exception.EntityNotFoundException;
+import com.github.resalner.javapractice.model.Product;
+import com.github.resalner.javapractice.repository.ProductRepository;
+import com.github.resalner.javapractice.request.ProductRequest;
+import java.util.List;
+
+@Service
+public class ProductServiceImpl {
+  @Autowired
+  private final ProductRepository productRepository;
+
+  public ProductService(ProductRepository productRepository){
+    this.productRepository = productRepository;
+  }
+
+  public List<Product> getProducts(){
+    return productRepository.findAll();
+  }
+
+  public void addProduct(ProductRequest productRequest){
+    Product product = new Product();
+    product.setName(productRequest.name());
+    product.setDescription(productRequest.description());
+    product.setPrice(productRequest.price());
+    product.setCategory_ID(productRequest.category_ID());
+    productRepository.save(product);
+  }
+
+  public Product getProduct(long id){
+    return productRepository.findById(id).orElseThrow(()->EntityNotFoundException("не найден продукт с id = "+id));
+  }
+
+  public void deleteProduct(long id){
+    productRepository.deleteById(id).orElseThrow(()->EntityNotFoundException("не найден продукт с id = "+id));
+  }
+  public Product updateProduct(long id, ProductRequest productRequest){
+    Product pr = productRepository.findById(id).orElseThrow(()->EntityNotFoundException("не найден продукт с id = "+id));
+    if(Objects.nonNull(productRequest.name()) && !"".equals(productRequest.name())){
+      pr.setName(productRequest.name());
+    }
+    if(Objects.nonNull(productRequest.description()) && !"".equals(productRequest.description())){
+      pr.setDescription(productRequest.description());
+    }
+    if(Objects.nonNull(productRequest.price()) && !"".equals(productRequest.price())){
+      pr.setPrice(productRequest.price());
+    }
+    if(Objects.nonNull(productRequest.category_ID()) && !"".equals(productRequest.category_ID())){
+      pr.setCategory_ID(productRequest.category_ID());
+    }
+    productRepository.save(pr);
+  }
+}
