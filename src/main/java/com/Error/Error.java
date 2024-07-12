9e45df05c8533b8eb5173309921de9a4857c4c4c
Aleksandr Putro <putroaleksandr85@gmail.com>
2024-07-12 16:01:19 +0300

    Добавлен "Packege"

--- src/main/java/com/Error/Error.java
+++ src/main/java/com/Error/Error.java
@@ -1,3 +1,5 @@
+package com.github.resalner.javapractice.error
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
