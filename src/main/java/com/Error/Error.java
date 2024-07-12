2c6d3714a88a5c398f2c006de01c50bd2d975be2
Aleksandr Putro <putroaleksandr85@gmail.com>
2024-07-12 16:02:55 +0300

    package

--- src/main/java/com/Error/Error.java
+++ src/main/java/com/Error/Error.java
@@ -1,4 +1,4 @@
-package com.github.resalner.javapractice.error
+package com.github.resalner.javapractice.error;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
