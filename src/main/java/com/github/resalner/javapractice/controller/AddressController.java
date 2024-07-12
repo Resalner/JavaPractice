4fb1e94ed7881b4d48ccbd54896e6876a00beab2
ilya <99881940+Kuchikamba@users.noreply.github.com>
2024-07-12 11:09:10 +0300

    Add files via upload

--- src/main/java/com/github/resalner/javapractice/controller/AddressController.java
+++ src/main/java/com/github/resalner/javapractice/controller/AddressController.java
@@ -7,41 +7,47 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import jakarta.validation.Valid;
 import com.github.resalner.javapractice.model.Address;
 import com.github.resalner.javapractice.repository.AddressRepository;
 import com.github.resalner.javapractice.request.AddressRequest;
-
 import com.github.resalner.javapractice.service.AddressService;
+import lombok.RequiredArgsConstructor;
+
 import java.util.List;
- 
+
 @RestController
-@RequestMapping(path = "/api/addresses" )
-public class AddressController{
-  private final AddressService addressService;
-  public AddressController(AddressService addressService){
-    this.addressService = addressService;
-  }
-// получение списка адресов
-  @GetMapping
-  public List<Address> getAddresses(){
-    return addressService.getAddresses();
-  } 
-// добавление нового адреса
-  @PostMapping("/addAddress")
-  public void saveAddress(@RequestBody @Valid AddressRequest addressRequest){
-    addressService.addAddress(addressRequest);
-  }
-  @GetMapping("/getAddress/{id}")
-  public Address getAddress(@PathVariable("id") long addressid){
-    return addressService.getAddress(addressid);
-  }
-  @DeleteMapping("/deleteAddress/{id}")
-  public void deleteAddress(@PathVariable("id") long addressid){
-    addressService.deleteAddress(addressid);
-  }
-  @PostMapping("/updateAddress/{id}")
-  public Address updateAddress(@PathVariable("id") long addressid, @RequestBody @Valid AddressRequest addressRequest){
-    return addressService.updateAddress(addressid, addressRequest);
-  }
+@RequestMapping(path = "/api/v1/addresses")
+@RequiredArgsConstructor
+public class AddressController {
+
+    @Autowired
+    private final AddressService addressService;
+
+    @GetMapping
+    public List<Address> getAddresses() {
+        return addressService.getAddresses();
+    }
+
+    @PostMapping("/addAddress")
+    public void saveAddress(@RequestBody @Valid AddressRequest addressRequest) {
+        addressService.addAddress(addressRequest);
+    }
+
+    @GetMapping("/{id}")
+    public Address getAddress(@PathVariable("id") long addressid) {
+        return addressService.getAddress(addressid);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAddress(@PathVariable("id") long addressid) {
+        addressService.deleteAddress(addressid);
+    }
+
+    @PutMapping("/{id}")
+    public Address updateAddress(@PathVariable("id") long addressid, @RequestBody @Valid AddressRequest addressRequest) {
+        return addressService.updateAddress(addressid, addressRequest);
+    }
 }
