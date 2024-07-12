1b655faa3ed51f2cbd6aaa08aecd8c802cd6708d
ilya <99881940+Kuchikamba@users.noreply.github.com>
2024-07-12 19:35:10 +0300

    Add files via upload

--- src/main/java/com/github/resalner/javapractice/service/UserService.java
+++ src/main/java/com/github/resalner/javapractice/service/UserService.java
@@ -2,12 +2,17 @@ package com.github.resalner.javapractice.service;
 
 import com.github.resalner.javapractice.model.User;
 import com.github.resalner.javapractice.request.UserRequest;
+
 import java.util.List;
 
-public interface UserService{
-  List<User> getAllUsers();
-  User getUser(long id);
-  void addUser(UserRequest userRequest);
-  void deleteUser(long id);
-  User updateUser(long id, UserRequest userRequest);
+public interface UserService {
+    List<User> getAllUsers();
+
+    User getUser(long id);
+
+    void addUser(UserRequest userRequest);
+
+    void deleteUser(long id);
+
+    User updateUser(long id, UserRequest userRequest);
 }
