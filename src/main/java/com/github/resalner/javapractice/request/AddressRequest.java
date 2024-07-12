3d5c02d051d87ba9dd5d18a667cae107be0b4daf
ilya <99881940+Kuchikamba@users.noreply.github.com>
2024-07-12 09:00:45 +0300

    Add files via upload

--- src/main/java/com/github/resalner/javapractice/request/AddressRequest.java
+++ src/main/java/com/github/resalner/javapractice/request/AddressRequest.java
@@ -6,20 +6,19 @@ import lombok.NoArgsConstructor;
 import jakarta.valiadation.constraints.NotBlank;
 
 
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class AddressRequest{
-  
-  @NotBlank(message = "Необходимо указать город")
-  private String city;
+public class AddressRequest {
 
-  @NotBlank(message = "Необходимо указать улицу")
-  private String street;
+    @NotBlank(message = "Необходимо указать город")
+    private String city;
 
-  @NotBlank(message = "Необходимо указать номер дома")
-  private String house_number;
+    @NotBlank(message = "Необходимо указать улицу")
+    private String street;
 
-  private String apartament_number;
+    @NotBlank(message = "Необходимо указать номер дома")
+    private String house_number;
+
+    private String apartament_number;
 }
