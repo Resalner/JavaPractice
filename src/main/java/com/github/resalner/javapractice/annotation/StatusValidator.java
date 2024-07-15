9e949b8ce9dff486d38f41e2bba204928816e6da
ilya <99881940+Kuchikamba@users.noreply.github.com>
2024-07-15 15:52:13 +0300

    Add files via upload

--- src/main/java/com/github/resalner/javapractice/annotation/StatusValidator.java
+++ src/main/java/com/github/resalner/javapractice/annotation/StatusValidator.java
@@ -7,15 +7,12 @@ import java.util.Arrays;
 import java.util.List;
 
 public class StatusValidator implements ConstraintValidator<StatusAnnotation, String> {
-    
-	@Override
-	public void initialize(StatusAnnotation constraintAnnotation) {
-	}
-	
-	@Override
-    public boolean isValid(String status, ConstraintValidatorContext context) {
-        List<String> Statuses = Arrays.asList("PENDING", "SHIPPED", "DELIVERED", "CANCELED");
-        return Statuses.contains(status);
+    @Override
+    public void initialize(StatusAnnotation constraintAnnotation) {
     }
 
+    @Override
+    public boolean isValid(String status, StatusAnnotation statusAnnotation) {
+        return status != null && status.matches("PENDING", "SHIPPED", "DELIVERED", "CANCELED");
+    }
 }
