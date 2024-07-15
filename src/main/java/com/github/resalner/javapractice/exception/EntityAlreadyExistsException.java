8d175f45d4e700007c6fadc92b9d0b2380b78afd
yani <yanazhuchok@gmail.com>
2024-07-15 20:54:48 +0300

    fix: formatted

--- src/main/java/com/github/resalner/javapractice/exception/EntityAlreadyExistsException.java
+++ src/main/java/com/github/resalner/javapractice/exception/EntityAlreadyExistsException.java
@@ -9,5 +9,4 @@ public class EntityAlreadyExistsException extends RuntimeException {
     public EntityAlreadyExistsException(String message) {
         super(message);
     }
-
 }
--- src/main/java/com/github/resalner/javapractice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/github/resalner/javapractice/exception/GlobalExceptionHandler.java
@@ -11,18 +11,21 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleEntityNotFoundException(EntityNotFoundException ex) {
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+        		.body(errorResponse);
     }
 
     @ExceptionHandler(EntityAlreadyExistsException.class)
     public ResponseEntity<ErrorResponse> handleEntityAlreadyExistsException(EntityAlreadyExistsException ex) {
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.CONFLICT, ex.getMessage());
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+        		.body(errorResponse);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex) {
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "An unexpected error occurred");
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+        		.body(errorResponse);
     }
 }
