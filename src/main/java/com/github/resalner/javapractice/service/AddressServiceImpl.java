1d7014978d71b02c06ba82971904907a8cc3a818
ilya <99881940+Kuchikamba@users.noreply.github.com>
2024-07-16 23:53:52 +0300

    Add files via upload

--- src/main/java/com/github/resalner/javapractice/service/AddressServiceImpl.java
+++ src/main/java/com/github/resalner/javapractice/service/AddressServiceImpl.java
@@ -1,72 +1,63 @@
-package com.github.resalner.javapractice.service;
+package com.github.resalner.javapractice.service.impl;
 
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.github.resalner.javapractice.exception.EntityNotFoundException;
 import com.github.resalner.javapractice.model.Address;
 import com.github.resalner.javapractice.repository.AddressRepository;
-import com.github.resalner.javapractice.request.AddressRequest;
 import com.github.resalner.javapractice.map.AddressMapper;
 import lombok.RequiredArgsConstructor;
-
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
-public class AddressServiceImpl implements AddressService {
+public class AddressServiceImpl implements AddressService{
+  
+  private final AddressRepository addressRepository;
 
-    private final AddressRepository addressRepository;
+  public List<Address> getAddresses(){
+    return addressRepository.findAll();
+  }
 
-    public List<Address> getAddresses() {
-        return addressRepository.findAll();
+  public void saveAddress(Address address){    
+    addressRepository.save(address);
+  }
+
+  public Address getAddress(long id){
+    return addressRepository.findById(id)
+      .orElseThrow(()->EntityNotFoundException("не найден адрес с id = "+id));
+  }
+  
+  public void deleteAddress(long id){
+    addressRepository.deleteById(id)
+      .orElseThrow(()->EntityNotFoundException("не найден адрес с id = "+id));
+  } 
+  
+  public Address updateAddress(long id, Address address){
+    Address ad = addressRepository.findById(id)
+      .orElseThrow(()->EntityNotFoundException("не найден адрес с id = "+id));
+    if(Objects.nonNull(address.city())
+       && !"".equals(address.city())){
+      
+      ad.setCity(address.city());
     }
-
-    public void saveAddress(AddressRequest addressRequest) {
-        Address address = new Address();
-        address.setCity(addressRequest.city());
-        address.setStreet(addressRequest.street());
-        address.setHouseNumber(addressRequest.houseNumber());
-        address.setApartamentNumber(addressRequest.apartamentNumber());
-        mapper.map(addressRequest, address);
+    if(Objects.nonNull(address.street())
+       && !"".equals(address.street())){
+      
+      ad.setStreet(address.street());
     }
-
-    public Address getAddress(long id) {
-        return addressRepository.findById(id)
-                .orElseThrow(() -> EntityNotFoundException("не найден адрес с id = " + id));
+    if(Objects.nonNull(address.houseNumber()) 
+       && !"".equals(address.houseNumber())){
+      
+      ad.setHouseNumber(address.houseNumber());
     }
-
-    public void deleteAddress(long id) {
-        addressRepository.deleteById(id)
-                .orElseThrow(() -> EntityNotFoundException("не найден адрес с id = " + id));
+    if(Objects.nonNull(address.apartamentNumber())
+       && !"".equals(address.apartamentNumber())){
+      
+      ad.setApartamentNumber(address.apartamentNumber());
     }
-
-    public Address updateAddress(long id, AddressRequest addressRequest) {
-        Address ad = addressRepository.findById(id)
-                .orElseThrow(() -> EntityNotFoundException("не найден адрес с id = " + id));
-        if (Objects.nonNull(addressRequest.city())
-                && !"".equals(addressRequest.city())) {
-
-            ad.setCity(addressRequest.city());
-
-        }
-        if (Objects.nonNull(addressRequest.street())
-                && !"".equals(addressRequest.street())) {
-
-            ad.setStreet(addressRequest.street());
-
-        }
-        if (Objects.nonNull(addressRequest.houseNumber())
-                && !"".equals(addressRequest.houseNumber())) {
-
-            ad.setHouseNumber(addressRequest.houseNumber());
-
-        }
-        if (Objects.nonNull(addressRequest.apartamentNumber())
-                && !"".equals(addressRequest.apartamentNumber())) {
-
-            ad.setApartamentNumber(addressRequest.apartamentNumber());
-
-        }
-        addressRepository.save(ad);
-    }
+    addressRepository.save(ad);
+    return
+  }
 }
