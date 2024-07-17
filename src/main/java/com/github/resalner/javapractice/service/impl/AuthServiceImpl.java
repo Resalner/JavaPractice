924894309aa470a39712363ea798ac94e83eadec
yani <yanazhuchok@gmail.com>
2024-07-17 17:29:34 +0300

    add: Persistable implementation

--- src/main/java/com/github/resalner/javapractice/service/impl/AuthServiceImpl.java
+++ src/main/java/com/github/resalner/javapractice/service/impl/AuthServiceImpl.java
@@ -14,25 +14,27 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class AuthServiceImpl implements AuthService{
-	
+public class AuthServiceImpl implements AuthService {
+
 	private final UserRepository userRepository;
 	private final UserInfoRepository userInfoRepository;
 	private final BCryptPasswordEncoder passwordEncoder;
-	
+
 	@Override
 	public void registerNewUserAccount(User user, UserInfo userInfo) {
-		if(usernameExists(user.getUsername())) {
-			throw new EntityAlreadyExistsException("Аккаунт с таким логином уже есть: "
-					+ user.getUsername());
+		if (usernameExists(user.getUsername())) {
+			throw new EntityAlreadyExistsException("Аккаунт с таким логином уже есть: " + user.getUsername());
 		}
-		
+
 		user.setPassword(passwordEncoder.encode(user.getPassword()));
-		
+
+		user.setNew(true);
+		userInfo.setNew(true);
+
 		userRepository.save(user);
 		userInfoRepository.save(userInfo);
 	}
-	
+
 	private boolean usernameExists(String username) {
 		return userRepository.findByUsername(username) != null;
 	}
