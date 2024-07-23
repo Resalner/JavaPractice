083fc6295380329c4ff48efbda59d4abfff71408
yani <yanazhuchok@gmail.com>
2024-07-23 14:13:06 +0300

    rename: SecUserDetails -> UserDetailsImpl

--- src/main/java/com/github/resalner/javapractice/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/github/resalner/javapractice/service/impl/UserDetailsServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.github.resalner.javapractice.config.SecUserDetails;
+import com.github.resalner.javapractice.config.UserDetailsImpl;
 import com.github.resalner.javapractice.model.User;
 import com.github.resalner.javapractice.repository.UserRepository;
 
@@ -16,17 +16,16 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @Service
-public class SecUserDetailsService implements UserDetailsService{
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService{
 
-	private UserRepository repository;
+	private final UserRepository repository;
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		Optional<User> user = repository.findByUsername(username);
-		return user.map(SecUserDetails :: new)
+		return user.map(UserDetailsImpl :: new)
 				.orElseThrow(() -> new EntityNotFoundException("Пользователь не найден: " + username));
 	}
 
